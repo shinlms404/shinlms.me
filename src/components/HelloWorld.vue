af9114be1053c547b24c025c94fcc967c710c239
shinlms404 <shinlms404@gmail.com>
2024-01-29 09:58:52 +0800

    chore: add taiwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,16 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 defineProps({
-  msg: String,
-})
+  msg: String
+});
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
